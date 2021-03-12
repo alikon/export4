705ca73271a7ac0d9fafa0bce6526104fcc5e275
Nicola Galgano <optimus4joomla@gmail.com>
2021-03-12 12:35:40 +0100

    2nd

--- content/content.php
+++ content/content.php
@@ -11,9 +11,8 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Http\HttpFactory;
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Log\Log;
 use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\CMS\Router\Route;
 use Joomla\Registry\Registry;
 
 /**
@@ -50,12 +49,20 @@ class PlgExportContent extends CMSPlugin
 	protected $db;
 
 	/**
+	 * URL to get the data.
+	 *
+	 * @var    string
+	 * @since  3.5
+	 */
+	protected $getUrl = '';
+
+	/**
 	 * URL to send the data.
 	 *
 	 * @var    string
 	 * @since  3.5
 	 */
-	protected $serverUrl = 'http://localhost/sit/api/index.php/v1/content/article';
+	protected $postUrl = '';
 
 	/**
 	 * First step to enter the sampledata. Content.
@@ -66,36 +73,44 @@ class PlgExportContent extends CMSPlugin
 	 */
 	public function onAjaxContent()
 	{
-		//$app = JFactory::getApplication();
 		$id  = $this->app->input->get('id');
-		$this->serverUrl = $this->params->get('url', 'http://localhost/sit/api/index.php/v1/content/article');
-		$key = $this->params->get('key');
+		$domain = $this->params->get('url', 'http://localhost');
+		$this->postUrl = $domain . '/api/index.php/v1/content/article';
+		$this->getUrl = $domain . '/api/index.php/v1/content';
+
 
 		// Get an instance of the generic articles model
 		JModelLegacy::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_content/models', 'ArticleModel');
 		$model = JModelLegacy::getInstance('Article', 'ContentModel', array('ignore_request' => true));
 
 		$item = $model->getItem($id);
-		$item->catid= 2;
+		$item->catid = $this->params->get('catid');
 		unset($item->created_by);
-		$content = json_encode($item);
 
-		$response = $this->sendData2($content);
-//print_r($response);
-//exit();
+		//var_dump($item);exit();
+		try
+		{
+			$response = $this->sendData($item);
+		}
+		catch (RuntimeException $e)
+		{
+			// There was an error sending data.
+			$this->app->redirect(Route::_('index.php?option=com_content&view=article&layout=edit&id=' . $id, false), ' Connection ', 'error');
+		}
+
 		if ($response->code !== 200)
 		{
 			$data = json_decode($response->body);
 
-			$this->app->redirect(JRoute::_('index.php?option=com_content&view=article&layout=edit&id=' . $id, false), $response->code . ' - ' . $data->errors[0]->title, 'error');
+			$this->app->redirect(Route::_('index.php?option=com_content&view=article&layout=edit&id=' . $id, false), $response->code . ' - ' . $data->errors[0]->title, 'error');
 		}
 
-		$this->app->redirect(JRoute::_('index.php?option=com_content&view=article&layout=edit&id=' . $id, false), 'Exported', 'success');
+		$this->app->redirect(Route::_('index.php?option=com_content&view=article&layout=edit&id=' . $id, false), 'Exported', 'success');
 
 	}
 
 	/**
-	 * Send the stats to the stats server
+	 * Send the data to the j4 server
 	 *
 	 * @return  boolean
 	 *
@@ -103,64 +118,39 @@ class PlgExportContent extends CMSPlugin
 	 *
 	 * @throws  RuntimeException  If there is an error sending the data.
 	 */
-	private function sendData($content)
+	private function sendData($item)
+	{
+		$response = $this->checkCategory($item->catid);
+		
+		if ($response->code !== 200)
+		{
+			return $response;
+		}
+
+		$content = json_encode($item);
+		$options = new Registry;
+		$options->set('Content-Type', 'application/json');
+		$headers = array('Authorization' => 'Bearer ' . $this->params->get('key'));
+
+		return  HttpFactory::getHttp($options)->post($this->postUrl, $content, $headers, 2);
+	}
+
+	/**
+	 * Check category existence
+	 *
+	 * @return  boolean
+	 *
+	 * @since   3.5
+	 *
+	 * @throws  RuntimeException  If there is an error sending the data.
+	 */
+	private function checkCategory($catid)
 	{
 		$options = new Registry;
 		$options->set('Content-Type', 'application/json');
-		$options->set('Authorization', 'API Key');
-		$options->set('X-Joomla-Token', 'c2hhMjU2OjY0NDplZTBiZTBiOTgwNWU2OGU2YWY2OTZkM2JmOTVjYzVjMmQ2OTg4NzNjMjIwYWM0ZmMxNzQ4OThjM2E4OGMyYWU4');
+		$headers = array('Authorization' => 'Bearer ' . $this->params->get('key'));
 
-		try
-		{
-			// Don't let the request take longer than 2 seconds to avoid page timeout issues
-			$response = HttpFactory::getHttp($options, 'curl')->post($this->serverUrl, $content, null, 4);
-		}
-		catch (UnexpectedValueException $e)
-		{
-			// There was an error sending stats. Should we do anything?
-			$msg = $e->getMessage();
-		}
-		catch (RuntimeException $e)
-		{
-			// There was an error connecting to the server or in the post request
-			$msg = $e->getMessage();
-		}
-		catch (Exception $e)
-		{
-			// An unexpected error in processing; don't let this failure kill the site
-			$msg = $e->getMessage();
-		}
- 
-		return $msg;
+		// Don't let the request take longer than 2 seconds to avoid page timeout issues
+		return HttpFactory::getHttp($options)->get($this->getUrl . '/categories/'. $catid, $headers, 2);
 	}
-
-	private function sendData2($content)
-	{
-		$curl = curl_init();
-		curl_setopt_array($curl, array(
-			CURLOPT_URL => $this->serverUrl,
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_ENCODING => '',
-			CURLOPT_MAXREDIRS => 10,
-			CURLOPT_TIMEOUT => 0,
-			CURLOPT_FOLLOWLOCATION => true,
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST => 'POST',
-			CURLOPT_POSTFIELDS => $content,
-			CURLOPT_HTTPHEADER => array(
-				'Content-Type: application/json',
-				'X-Joomla-Token: c2hhMjU2OjY0NDplZTBiZTBiOTgwNWU2OGU2YWY2OTZkM2JmOTVjYzVjMmQ2OTg4NzNjMjIwYWM0ZmMxNzQ4OThjM2E4OGMyYWU4'
-			),
-		));
-
-		$data = new stdClass;
-		$data->body = curl_exec($curl);
-		$data->code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-		curl_close($curl);
-	//	print_r($response);
-	//	exit();
-		return $data;
-	}
-
 }
